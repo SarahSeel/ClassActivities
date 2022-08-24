ea413cec4e6c6a1b47c0172b068827661292437a
SarahSeel <sarahizsilly09@yahoo.com>
2022-08-24 14:01:20 -0500

    2.2.2-JSVariablesAndPrimitives.js

---
+++ Section02/2.2/2.2.2-JSVariablesAndPrimitives.js
@@ -0,0 +1,46 @@
+// create a variable called "name" that references a string
+let name = "Sarah";
+
+// create a variable called "favoriteSong" that references a string
+let favoriteSong = "Down";
+
+// create a variable called "wage" that references a number
+let wage = 2;
+
+// create a variable called "age" that references a number
+let age = 31;
+
+// create a variable called "onlyChild" that references a boolean
+let onlyChild = false;
+
+// create a variable called "satisfied" that references a boolean
+let satisfied = true;
+
+// create a variable called "favoriteThing" that references ANY primitive value
+let favoriteThing = "Unicorns";
+
+// print the data type of "favoriteThing"
+console.log(favoriteThing);
+console.log(typeof favoriteThing);
+
+// You heard a new song that became your favorite.
+// Reassign the variable "favoriteSong"
+favoriteSong = "What's my Age Again";
+
+// You no longer like the type of data stored in "favoriteThing"
+// Reassign "favoriteThing" to a different primitive data type and print its type
+favoriteThing = 5;
+console.log(typeof favoriteThing);
+
+// Happy Birthday!
+// Reassign "age" to a new value
+age = 10;
+
+// You got a big raise at work.
+// Reassign "wage" to a new value
+wage = 44;
+// Your raise changed your job satisfaction.
+// Assign "satisfied" to true
+satisfied = true;
+// use "console.log()" and "+" to print a sentence that includes two of the above variables
+console.log("I am" + age + " and I make " + wage + "dollars per hour");
