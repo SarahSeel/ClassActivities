28410adf3dafba54ae5556ba192b81b37f34314b
SarahSeel <sarahizsilly09@yahoo.com>
2022-08-30 18:22:54 -0500

    3.1

---
+++ Section03/3.1/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,40 @@
+// TODO: using the function expressions below, refactor them into function declarations
+
+// Greet Function
+const greet = function() {
+    console.log("Hello!");
+  };
+
+  function greet(){
+    console.log("Hello!");
+  }
+  greet();
+// threeModTwo Function
+
+  const threeModTwo = function() {
+    console.log(3 % 2);
+  };
+function threeModTwo(){
+  console.log(3 % 2);
+}
+// threeModTwo ();
+
+let age = 18;
+
+// CheckDrivingAgeFunction
+
+const checkDrivingAge = function() {
+    if (age >= 16) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
+
+  function checkDrivingAge() {
+    if (age >= 16) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  };
---
+++ Section03/3.1/3.1.2-FunctionInvocation.js
@@ -0,0 +1,16 @@
+// print the function below
+function myNumberFunction() {
+  let num = 2 ** 2; //4
+  num = num + 3; //7
+  num = num % 4; //3
+  num = num * 14; //42
+  console.log(num);
+}
+// print the function above
+console.log(myNumberFunction);
+
+// now, invoke the myNumberFunction function
+myNumberFunction();
+
+// print just the function of console.log (no invocation)
+console.log(console.log);
